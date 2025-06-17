4e64da2002e42ea0cf01a565093f42f2d510a14f
Luisferha <lhernandeza@ucenfotec.ac.cr>
2025-06-16 21:00:05 -0600

    pagina Luis

---
+++ src/app/pages/luis/luis.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-luis',
+  standalone: true,
+  imports: [],
+  templateUrl: './luis.component.html',
+  styleUrl: './luis.component.scss'
+})
+export class LuisComponent {
+
+}
